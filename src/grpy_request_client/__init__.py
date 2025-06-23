dcdf5dccac1ad680a7399d4336ef61d06892db76
brooksjbr <brooksjbr@protonmail.ch>
2025-06-23 10:05:01 -0400

    chore: move handler to manager

--- src/grpy_request_client/__init__.py
+++ src/grpy_request_client/__init__.py
@@ -1,6 +1,6 @@
 from src._version import __version__
 from src.grpy_request_client.factories.request_session_factory import RequestSessionFactory
-from src.grpy_request_client.handlers.request_handler import RequestHandler
+from src.grpy_request_client.managers.request_manager import RequestManager
 from src.grpy_request_client.models.request_model import RequestModel
 
-__all__ = ["RequestSessionFactory", "RequestHandler", "RequestModel", "__version__"]
+__all__ = ["RequestSessionFactory", "RequestManager", "RequestModel", "__version__"]
