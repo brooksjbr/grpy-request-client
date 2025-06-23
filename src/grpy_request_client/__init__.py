ccb82392c3f8af99c9d1fb30ca1251e616d78dc3
brooksjbr <brooksjbr@protonmail.ch>
2025-06-23 10:18:45 -0400

    chore: factory to manager

--- src/grpy_request_client/__init__.py
+++ src/grpy_request_client/__init__.py
@@ -1,6 +1,6 @@
 from src._version import __version__
-from src.grpy_request_client.factories.request_session_factory import RequestSessionFactory
 from src.grpy_request_client.managers.request_manager import RequestManager
+from src.grpy_request_client.managers.session_manager import SessionManager
 from src.grpy_request_client.models.request_model import RequestModel
 
-__all__ = ["RequestSessionFactory", "RequestManager", "RequestModel", "__version__"]
+__all__ = ["SessionManager", "RequestManager", "RequestModel", "__version__"]
